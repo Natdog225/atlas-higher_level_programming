4855838944100d212648a6d217dfb60088f34c49
Natdog225 <Nathaniel.wilson@atlasschool.com>
2024-08-28 02:36:38 +0000

    .

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -3,7 +3,3 @@ str = "Python is an interpreted, interactive, object-oriented programming\ langu
 word1 = str.split(", ")[2].split()[0] # Extract "object-oriented"
 word2 = str.split()[0] # Extract "Python"
 print(f"{word1} programming with {word2}")
-
-
-
-
