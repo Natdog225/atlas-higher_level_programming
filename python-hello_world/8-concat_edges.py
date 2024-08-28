2d522d9067a5f1943dcd690beea22b4abd17cf87
Natdog225 <Nathaniel.wilson@atlasschool.com>
2024-08-28 02:42:34 +0000

    fixed maybe?

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming\ language that combines remarkable power with very clear syntax" 
-word1 = str.split(", ")[2].split()[0] # Extract "object-oriented"
-word2 = str.split()[0] # Extract "Python"
-print(f"{word1} programming with {word2}")
+str = "Python is an interpreted, interactive, object-oriented programming\ language that combines remarkable power with very clear syntax"
+str = str[39:66] + str[106:112] + str[0:6]
+print(str)
