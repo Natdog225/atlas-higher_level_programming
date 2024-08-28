6c05baf07e53b9116776bfab1bdfac028f601969
Natdog225 <Nathaniel.wilson@atlasschool.com>
2024-08-28 02:35:03 +0000

    added a space after "

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming\language that combines remarkable power with very clear syntax" 
+str = "Python is an interpreted, interactive, object-oriented programming\ language that combines remarkable power with very clear syntax" 
 word1 = str.split(", ")[2].split()[0] # Extract "object-oriented"
 word2 = str.split()[0] # Extract "Python"
 print(f"{word1} programming with {word2}")
