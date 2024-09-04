3276e102823fc34fc8403546768fa6c56e59f04b
root <Nathaniel.wilson@atlasschool.com>
2024-09-04 01:06:25 +0000

    ideally fixed no 6 to be aligned with checker rules.

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
 # Print all possible combinations of two digits
-for i in range(10):
-    for j in range(i + 1, 10):  # Start from i+1 to avoid dups
-        print(f"{i:d}{j:d}", end=", " if i < 8 or j < 9 else "\n")
+for i in range(8):
+    print(", ".join(f"{i:d}{j:d}" for j in range(i + 1, 10)), end=", ")
+
+# Last row (89)
+print(", ".join(f"{8:d}{j:d}" for j in range(9, 10)))
