455d19b7db4831058a361b3ce10553257a20ea32
root <Nathaniel.wilson@atlasschool.com>
2024-09-04 02:56:56 +0000

    fixed line in 10

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -7,7 +7,8 @@ def divisible_by_2(my_list=[]):
         my_list: The list of integers.
 
     Returns:
-        A new list with True or False for each element, indicating divisibility by 2.
+        A new list with True or False for each element, 
+        indicating divisibility by 2.
     """
 
     return [num % 2 == 0 for num in my_list]
