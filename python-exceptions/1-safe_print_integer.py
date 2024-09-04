ae57da4cee04fc732260f3ae3358f4af31ee9a52
root <Nathaniel.wilson@atlasschool.com>
2024-09-04 04:35:52 +0000

    added some lines to make the spaces go away

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,8 +1,22 @@
 #!/usr/bin/python3
 
-def safe_print_integer(value):
+def safe_print_list(my_list=[], x=0):
+    count = 0
     try:
-        print("{:d}".format(value))
-        return True
-    except (ValueError, TypeError):
-        return False
+        for i in range(x):
+            print(my_list[i], end="")  # Remove the space after printing each element
+            count += 1
+    except IndexError:
+        pass 
+    finally:
+        print()  # Print a new line after the loop
+        return count
+
+my_list = [1, 2, 3, 4, 5]
+
+nb_print = safe_print_list(my_list, 2)
+print("nb_print: {:d}".format(nb_print))
+nb_print = safe_print_list(my_list, len(my_list))
+print("nb_print: {:d}".format(nb_print))
+nb_print = safe_print_list(my_list, len(my_list) + 2)
+print("nb_print: {:d}".format(nb_print))
