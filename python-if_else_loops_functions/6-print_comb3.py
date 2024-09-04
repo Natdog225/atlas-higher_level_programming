5c80cc68c90f8e3e1e8429a57c0678df353b66fe
root <Nathaniel.wilson@atlasschool.com>
2024-09-04 01:08:15 +0000

    no 6 fixes again.

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 
 # Print all possible combinations of two digits
+# First 8 rows (00 to 79)
 for i in range(8):
-    print(", ".join(f"{i:d}{j:d}" for j in range(i + 1, 10)), end=", ")
+    print(", ".join("{:d}{:d}".format(i, j) for j in range(i + 1, 10)), end=", ")
 
 # Last row (89)
-print(", ".join(f"{8:d}{j:d}" for j in range(9, 10)))
+print(", ".join("{:d}{:d}".format(8, j) for j in range(9, 10)))
