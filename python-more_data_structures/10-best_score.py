723391293b51703ca204de758346c85b1d4fcd56
root <Nathaniel.wilson@atlasschool.com>
2024-09-04 04:03:58 +0000

    fixed line len in 10

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def best_score(a_dictionary):
-    if a_dictionary is None or not a_dictionary:  # Check if dictionary is None or empty
+    if a_dictionary is None or not a_dictionary:
         return None
 
     best_key = None
