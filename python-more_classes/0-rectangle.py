8ac03fc956075e1e2680b8cab3e3895493dfb6bb
Natdog225 <Nathaniel.wilson@atlasschool.com>
2024-09-09 23:14:37 -0500

    Bones for the project

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+This module defines an empty class Rectangle representing a rectangle.
+"""
+
+
+class Rectangle:
+    """An empty class that defines a rectangle."""
+    pass
---
+++ python-more_classes/README.md
@@ -0,0 +1,1 @@
+This project is apparently about rectangles and THATS IT. NOTHING ELSE. AINT NO SQUARES HERE
