fe70b55e9e5426f7702dee4ae3bf82a33700e2bd
Natdog225 <Nathaniel.wilson@atlasschool.com>
2024-09-16 22:13:33 -0500

    It did not like that.

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,42 +1,10 @@
 #!/usr/bin/python3
-"""Defines the MyList class, inheriting from list.
-
-Example:
-    >>> my_list = MyList()
-    >>> my_list.append(1)
-    >>> my_list.append(4)
-    >>> my_list.append(2)
-    >>> my_list.append(3)
-    >>> my_list.append(5)
-    >>> print(my_list)
-    [1, 4, 2, 3, 5]
-    >>> my_list.print_sorted()
-    [1, 2, 3, 4, 5]
-    >>> print(my_list)
-    [1, 4, 2, 3, 5]
-
-"""
+"""Defines the MyList class, inheriting from list."""
 
 
 class MyList(list):
-    """A subclass of list that has a print_sorted method.
-
-    Example:
-        >>> my_list = MyList()
-        >>> my_list.extend([3, 1, 4, 2])
-        >>> my_list.print_sorted()
-        [1, 2, 3, 4]
-
-    """
+    """A subclass of list that has a print_sorted method."""
 
     def print_sorted(self):
-        """Prints the list elements in ascending sorted order.
-
-        Example:
-            >>> my_list = MyList()
-            >>> my_list.extend([3, 1, 4, 2])
-            >>> my_list.print_sorted()
-            [1, 2, 3, 4]
-
-        """
+        """Prints the list elements in ascending sorted order."""
         print(sorted(self))
