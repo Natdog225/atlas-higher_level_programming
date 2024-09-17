6d7e951f04a54c5414582206f7eeef2903c1d1e2
Natdog225 <Nathaniel.wilson@atlasschool.com>
2024-09-16 22:11:30 -0500

    Trying to throw the doctest stuff into the file directly to appease checker.

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,10 +1,42 @@
 #!/usr/bin/python3
-"""Defines the MyList class, inheriting from list."""
+"""Defines the MyList class, inheriting from list.
+
+Example:
+    >>> my_list = MyList()
+    >>> my_list.append(1)
+    >>> my_list.append(4)
+    >>> my_list.append(2)
+    >>> my_list.append(3)
+    >>> my_list.append(5)
+    >>> print(my_list)
+    [1, 4, 2, 3, 5]
+    >>> my_list.print_sorted()
+    [1, 2, 3, 4, 5]
+    >>> print(my_list)
+    [1, 4, 2, 3, 5]
+
+"""
 
 
 class MyList(list):
-    """A subclass of list that has a print_sorted method."""
+    """A subclass of list that has a print_sorted method.
+
+    Example:
+        >>> my_list = MyList()
+        >>> my_list.extend([3, 1, 4, 2])
+        >>> my_list.print_sorted()
+        [1, 2, 3, 4]
+
+    """
 
     def print_sorted(self):
-        """Prints the list elements in ascending sorted order."""
+        """Prints the list elements in ascending sorted order.
+
+        Example:
+            >>> my_list = MyList()
+            >>> my_list.extend([3, 1, 4, 2])
+            >>> my_list.print_sorted()
+            [1, 2, 3, 4]
+
+        """
         print(sorted(self))
