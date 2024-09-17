27ec10860b1718cfebaacba712acbbecafe0d8cb
Natdog225 <Nathaniel.wilson@atlasschool.com>
2024-09-17 14:43:55 -0500

    ""

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -24,7 +24,7 @@ class Student:
         if attrs is None:
             return self.__dict__
         else:
-            return {key: value for key, value in self.__dict__.items() 
+            return {key: value for key, value in self.__dict__.items()
                     if key in attrs}
 
     def reload_from_json(self, json):
