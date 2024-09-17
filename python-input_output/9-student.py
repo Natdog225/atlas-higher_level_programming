ec84377a2d563e5b41534d6e3356a7cecbba29ed
Natdog225 <Nathaniel.wilson@atlasschool.com>
2024-09-17 14:19:08 -0500

    Attempted fixes.

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -1,22 +1,19 @@
-#!/usr/bin/python3
-"""Module that contains the Student class"""
-
-
 class Student:
-    """A class representing a student. Just a regular ol' student"""
+    """
+    Represents a student.
+    """
 
     def __init__(self, first_name, last_name, age):
-        """Initializes a student, to school
-
-        Args:
-        first_name (str): Sometimes I feel like these are too obvious
-        last_name (str): The GUESS WHAT, last name.
-        age (int): The age
+        """
+        Initializes a student with first name, last name, and age.
         """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
-        def to_json(self):
-            """Retrieves a dictionary representation of a student"""
-            return self.__dict__
+    def to_json(self):
+        """
+        Retrieves a dictionary representation of a Student instance.
+        """
+        return self.__dict__
+    
