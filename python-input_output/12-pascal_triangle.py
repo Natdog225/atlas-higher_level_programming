ba52f7c6de0e71191705bec5f8058bbaed8c7d53
Natdog225 <Nathaniel.wilson@atlasschool.com>
2024-09-17 14:47:47 -0500

    ""

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -1,28 +1,29 @@
 #!/usr/bin/python3
 """Module that contains the pascal_triangle function"""
 
+
 def pascal_triangle(n):
-	"""Returns a list of lists of ints that might resemble Pascal's
-	triangle.
+    """Returns a list of lists of integers representing
+    the Pascal’s triangle of n
 
-	Args: n (int): The number of rows in the triangle
+    Args:
+        n (int): The number of rows in the triangle
 
-	Returns:
-	A list of list resembling a triangle.
-	"""
+    Returns:
+        A list of lists representing Pascal's triangle
+    """
 
-	if n <= 0:
-		return []
-	
-	triangle = [[1]]
+    if n <= 0:
+        return []
 
-	for i in range(1, n):
-		row = [1]
-		prev_row = triangle[i - 1]
-		for j in range(1, i):
-			row.append(prev_row[j - 1] + prev_row[j])
-			row.append(1)
-			triangle.append(row)
+    triangle = [[1]]  # Initialize with the first row
 
-		return triangle
+    for i in range(1, n):
+        row = [1]  # Each row starts with 1
+        prev_row = triangle[i - 1]
+        for j in range(1, i):
+            row.append(prev_row[j - 1] + prev_row[j])
+        row.append(1)  # Each row ends with 1
+        triangle.append(row)  # Append the completed row to the triangle
 
+    return triangle
