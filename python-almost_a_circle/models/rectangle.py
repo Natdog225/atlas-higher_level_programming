574b1ef846ae1ac127ebd0c8a349598159548217
Natdog225 <Nathaniel.wilson@atlasschool.com>
2024-09-19 15:39:07 -0500

    Ideally doing the thing with the display setting.

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -125,8 +125,10 @@ class Rectangle(Base):
         """
         Prints the rectangle to stdout using '#'
         """
+        for _ in range(self.__y):
+            print()
         for _ in range(self.__height):
-            print("#" * self.__width)
+            print(" " * self.__x + "#" * self.__width)
 
     def __str__(self):
         """
@@ -135,3 +137,4 @@ class Rectangle(Base):
         return "[Rectangle] ({}) {}/{} - {}/{}".format(
             self.id, self.__x, self.__y, self.__width, self.__height
         )
+    
