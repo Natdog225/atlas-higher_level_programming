7be7830d868b077ecb24c3f1a1d1ec9658f9ec95
Natdog225 <Nathaniel.wilson@atlasschool.com>
2024-09-22 12:47:12 -0500

    ..

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -53,3 +53,14 @@ class Base:
         if json_string is None or json_string == "":
             return []
         return json.loads(json_string)
+
+def to_json_string(list_dictionaries):
+    """
+    Returns the JSON string representation of list_dictionaries
+
+    Args:
+        list_dictionaries (list): A list of dictionaries
+    """
+    if list_dictionaries is None or list_dictionaries == []:
+        return "[]"
+    return json.dumps(list_dictionaries)
