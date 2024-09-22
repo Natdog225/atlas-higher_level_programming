658bd78b45a9befaac6c07355e8e492ca5deb4dd
Natdog225 <Nathaniel.wilson@atlasschool.com>
2024-09-22 12:45:28 -0500

    Updated base. I think this is gonna be the last one I do today for a bit cause my food just got here.

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -16,10 +16,41 @@ class Base:
         Initializes a `Base` object
 
         Args:
-            id (int): The id of the object. If `None`, an auto id is assigned
+            id (int): The id of the object.
+              If `None`, an auto-incrementing id is assigned
         """
         if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """
+        Writes the JSON string representation of list_objs to a file
+
+        Args:
+            list_objs (list): A list of instances who inherits of Base
+        """
+        filename = cls.__name__ + ".json"
+        with open(filename, "w") as f:
+            if list_objs is None:
+                f.write("[]")
+            else:
+                list_dicts = [o.to_dictionary() for o in list_objs]
+                f.write(json.dumps(list_dicts))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """
+        Returns the list of the JSON string representation json_string
+
+        Args:
+            json_string (str): A JSON string representation
+              of a list of dictionaries
+        """
+        if json_string is None or json_string == "":
+            return []
+        return json.loads(json_string)
+    
