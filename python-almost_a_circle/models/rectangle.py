98abd92570363c98ab5ed75ea842732e437a2a31
Natdog225 <Nathaniel.wilson@atlasschool.com>
2024-09-22 11:32:33 -0500

    Attempting at assigning attributes. Also updated module comments.

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -19,8 +19,7 @@ class Rectangle(Base):
             height (int): The height of the rectangle
             x (int): The x coordinate of the rectangle
             y (int): The y coordinate of the rectangle
-            id (int): The id of the rectangle. If `None`,
-              an auto-incrementing id is assigned
+            id (int): The id of the rectangle. If `None`, an auto-incrementing id is assigned
         """
         super().__init__(id)
         self.width = width
@@ -137,3 +136,16 @@ class Rectangle(Base):
         return "[Rectangle] ({}) {}/{} - {}/{}".format(
             self.id, self.__x, self.__y, self.__width, self.__height
         )
+
+    def update(self, *args):
+        """
+        Updates the attributes of the rectangle
+
+        Args:
+            *args: Variable length argument list
+        """
+        if args:
+            attrs = ["id", "width", "height", "x", "y"]
+            for i, arg in enumerate(args):
+                if i < len(attrs):
+                    setattr(self, attrs[i], arg)
