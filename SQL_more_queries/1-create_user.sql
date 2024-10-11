a5ae29335503f9f3708d90464c70315700522116
Natdog225 <Nathaniel.wilson@atlasschool.com>
2024-10-11 16:34:51 -0500

    First couple

---
+++ SQL_more_queries/1-create_user.sql
@@ -0,0 +1,3 @@
+-- Creates the user `user_0d_1` with all privileges on the server.
+CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
