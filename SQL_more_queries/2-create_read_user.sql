c132419ede5cf1f25d948f7904a6314720ac0e93
Natdog225 <Nathaniel.wilson@atlasschool.com>
2024-10-11 16:57:43 -0500

    Fixes for 1 and 2

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,6 +1,6 @@
--- Creates the database `hbtn_0d_2` if it doesn't exist.
+-- Creates the database hbtn_0d_2.
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 
--- Creates the user `user_0d_2` with the specified password and grants SELECT privileges on the database `hbtn_0d_2`.
-CREATE USER 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+-- Creates the user user_0d_2.
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
