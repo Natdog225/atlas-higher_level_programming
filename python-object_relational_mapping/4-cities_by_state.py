35bf9b120b684162a51bb16b6f797166ece6791e
Natdog225 <Nathaniel.wilson@atlasschool.com>
2024-10-14 17:16:38 -0500

    seperated line

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -22,7 +22,8 @@ def list_cities():
                          port=3306)
 
     cur = db.cursor()
-    cur.execute("SELECT cities.id, cities.name, states.name FROM cities, states WHERE cities.state_id = states.id ORDER BY cities.id ASC")
+    cur.execute("SELECT cities.id, cities.name, \
+                states.name FROM cities, states WHERE cities.state_id = states.id ORDER BY cities.id ASC")
     rows = cur.fetchall()
     for row in rows:
         print(row)
