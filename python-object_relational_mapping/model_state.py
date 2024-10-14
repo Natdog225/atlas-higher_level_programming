a1a13b7b1cba4390b373b1d26870d4e9a12315b8
Natdog225 <Nathaniel.wilson@atlasschool.com>
2024-10-14 17:30:25 -0500

    Fix for 6

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,18 +1,20 @@
 #!/usr/bin/python3
 """
-This is a model of the state to base other things off of
+This module defines the State class.
 """
-from sqlalchemy import create_engine
+
+import sqlalchemy
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
+
 class State(Base):
+    """
+    State class that maps to the `states` table in the database.
+    """
     __tablename__ = 'states'
 
-    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
-    name = sqlalchemy.Column(sqlalchemy.String(128), nullable=False)
-
-if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(128), nullable=False)
