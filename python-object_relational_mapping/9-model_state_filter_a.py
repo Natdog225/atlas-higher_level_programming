2538118e75e03bfd71bf4b2b55505fbb56da0ef7
Natdog225 <Nathaniel.wilson@atlasschool.com>
2024-10-14 17:54:05 -0500

    9 fix. Sessionmaker is weird.

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
-This module defines a function that lists all State objects that
-contain the letter 'a' from a database
+This module defines a function that lists all State objects that contain
+the letter 'a' from a database.
 """
 
 import sys
@@ -12,11 +12,15 @@ from sqlalchemy.orm import sessionmaker
 
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
 
-    session = sessionmaker(bind=engine)()
-    for state in session.query(State).filter(State.name.like('%a%')).order_by(State.id):
+    Session = sessionmaker(bind=engine)  # Create a Session class
+    session = Session()                 # Create a session object
+
+    for state in session.query(State).filter(State.name.like('%a%'))\
+        .order_by(State.id):
         print("{}: {}".format(state.id, state.name))
 
     session.close()
