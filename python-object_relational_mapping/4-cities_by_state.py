afe3f774d86020b2cfd1775d6548595d172a0635
Natdog225 <Nathaniel.wilson@atlasschool.com>
2024-10-14 17:19:14 -0500

    lots of seperation

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -23,7 +23,8 @@ def list_cities():
 
     cur = db.cursor()
     cur.execute("SELECT cities.id, cities.name, \
-                states.name FROM cities, states WHERE cities.state_id = states.id ORDER BY cities.id ASC")
+                states.name FROM cities, states WHERE cities.state_id \
+                    = states.id ORDER BY cities.id ASC")
     rows = cur.fetchall()
     for row in rows:
         print(row)
