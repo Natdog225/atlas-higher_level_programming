7c8c85e5512d9d89b406109f119b852dbb69ebe6
Natdog225 <Nathaniel.wilson@atlasschool.com>
2024-10-16 11:19:42 -0500

    fix

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -8,32 +8,6 @@ import sys
 from model_state import Base, State
 from model_city import City
 
-from sqlalchemy import (create_engine)
-from sqlalchemy.orm import sessionmaker
-
-
-if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
-
-    Session = sessionmaker(bind=engine)  # Create a Session class
-    session = Session()                 # Create a session object
-
-    for city in session.query(City).order_by(City.id).all():
-        print("{}: ({}) {}".format(city.state.name, city.id, city.name))
-
-    session.close()
-#!/usr/bin/python3
-"""
-This module defines a function that prints all City objects from
-a database.
-"""
-
-import sys
-from model_state import Base, State
-from model_city import City
-
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
