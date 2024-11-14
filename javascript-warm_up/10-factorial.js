58b73cb1082ccfcc134c8b16ac5062507205cb48
Natdog225 <Nathaniel.wilson@atlasschool.com>
2024-11-14 15:49:28 -0600

    updated 10

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,9 +1,14 @@
 #!/usr/bin/node
 
 function factorial(n) {
-    if (isNaN(n) || n <= 1) {
-        return 1;
-    } else {
-        return n * factorial(n - 1);
-    }
+  if (isNaN(n) || n <= 1) {
+    return 1;
+  } else {
+    const result = n * factorial(n - 1);
+    console.log(result);
+    return result;
+  }
 }
+
+const num = parseInt(process.argv[2]);
+factorial(num);
