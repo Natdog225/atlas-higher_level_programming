b3366d2682e15b28309f4a0fa96a26de06a82584
Natdog225 <Nathaniel.wilson@atlasschool.com>
2024-11-14 15:42:00 -0600

    7 fixes

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -1,10 +1,13 @@
 #!/usr/bin/node
-const numOccurrences = parseInt(process.arg[2]);
+
+const numOccurrences = parseInt(process.argv[2]);
 
 if (isNaN(numOccurrences)) {
-    console.log('Missing number of occurrences');
+  console.log('Missing number of occurrences'); 
 } else {
-    for (let i = 0; i < numOccurrences; i++) {
-        console.log('C is fun');
-    }
+  let result = '';
+  for (let i = 0; i < numOccurrences; i++) {
+    result += 'C is fun\n'; 
+  }
+  console.log(result); 
 }
