e5168127748f9e8fdb747852992b58f85103260c
Natdog225 <Nathaniel.wilson@atlasschool.com>
2024-11-14 15:50:49 -0600

    had to move console.log cause it was printing every step

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -4,11 +4,9 @@ function factorial(n) {
   if (isNaN(n) || n <= 1) {
     return 1;
   } else {
-    const result = n * factorial(n - 1);
-    console.log(result);
-    return result;
+    return n * factorial(n - 1);
   }
 }
 
 const num = parseInt(process.argv[2]);
-factorial(num);
+console.log(factorial(num));
