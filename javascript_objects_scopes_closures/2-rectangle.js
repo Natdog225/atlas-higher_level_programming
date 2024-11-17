693330ce8ae7708f0f5272c30f9a6dc7ff684a01
Natdog225 <Nathaniel.wilson@atlasschool.com>
2024-11-16 22:05:02 -0600

    stand

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
 class Rectangle {
-  constructor(w, h) {
-    if (w > 0 && h > 0) { // Check if w and h are positive integers
+  constructor (w, h) {
+    if (w > 0 && h > 0) { // Check if positive
       this.width = w;
       this.height = h;
     }
