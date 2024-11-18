03e679d64d57c33b57bd2d01b4aebc83542aa565
Natdog225 <Nathaniel.wilson@atlasschool.com>
2024-11-18 13:06:02 -0600

    stand?

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -1,12 +1,4 @@
 #!/usr/bin/node
 
-class Rectangle {
-  constructor (w, h) {
-    if (w > 0 && h > 0) { // Check if positive
-      this.width = w;
-      this.height = h;
-    }
-  }
-}
 
 module.exports = class Rectangle {};
