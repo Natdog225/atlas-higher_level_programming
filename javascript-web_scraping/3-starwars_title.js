32d6b073bc7feece2c1b0992d83504f141da34e1
Natdog225 <Nathaniel.wilson@atlasschool.com>
2024-11-26 21:58:09 -0600

    stand fixes

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
-// Include the request module for making HTTP requests
 const request = require('request');
 
-// Get the movie ID
+// Get the movie ID from the command line arguments
 const movieId = process.argv[2];
 
 // Construct the API URL with the movie ID
@@ -11,15 +10,15 @@ const apiUrl = `https://swapi-api.hbtn.io/api/films/${movieId}`;
 // Make a GET request to the Star Wars API
 request(apiUrl, (error, response, body) => {
   if (error) {
-    // If an error occurs during the request, print the error
+    // If an error occurs, print the error
     console.error(error);
   } else {
-    // If the request is successful, parse and print the title
+    // If the request is successful, parse the JSON response and print the title
     try {
       const movieData = JSON.parse(body);
       console.log(movieData.title);
     } catch (parseError) {
-      console.error('Error parsing JSON:', parseError); 1 
+      console.error('Error parsing JSON:', parseError);
     }
   }
 });
